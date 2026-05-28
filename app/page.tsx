02e81c59cb6c5629f9012523a228954ae922ab73
Natrodko <nataliekolarovaa@gmail.com>
2026-05-28 20:01:38 +0000

    Natalie, Timer

--- app/page.tsx
+++ app/page.tsx
@@ -61,6 +61,18 @@ export default function Home() {
                 · a yellow button, a counter, and the joy of clicking
               </span>
             </li>
+            <li>
+              <a
+                href="/mike/index.html"
+                className="text-zinc-900 underline decoration-zinc-300 underline-offset-4 hover:decoration-zinc-900"
+              >
+                Mike's Click Game
+              </a>
+              <span className="text-zinc-500">
+                {" "}
+                · a yellow button, a counter, and the joy of clicking
+              </span>
+            </li>
           </ul>
         </div>
       </div>
